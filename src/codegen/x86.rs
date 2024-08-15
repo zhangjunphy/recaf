747fd3a637b96be1b5ba0230d34e98c865d441df
jzhang <jun.zhang@amd.com>
2024-08-15 18:17:44 +0800

    working on codegen to x86 assembly

---
+++ src/codegen/x86.rs
@@ -0,0 +1,84 @@
+pub struct Imm(u64);
+pub struct Mem(u64);
+pub enum Reg {
+    RAX,
+    RBX,
+    RCX,
+    RDX,
+    RSP,
+    RBP,
+    RSI,
+    RDI,
+    R(u64),
+}
+
+impl From<u64> for Reg {
+    fn from(i: u64) -> Self {
+        match i {
+            0 => Self::RAX,
+            1 => Self::RBX,
+            2 => Self::RCX,
+            3 => Self::RDX,
+            4 => Self::RSP,
+            5 => Self::RBP,
+            6 => Self::RSI,
+            7 => Self::RDI,
+            i if i > 7 && i < 16 => Self::R(i),
+            _ => panic!("Register idx out of range: {}", i),
+        }
+    }
+}
+
+impl Into<u64> for Reg {
+    fn into(self) -> u64 {
+        match self {
+            Self::RAX => 0,
+            Self::RBX => 1,
+            Self::RCX => 2,
+            Self::RDX => 3,
+            Self::RSP => 4,
+            Self::RBP => 5,
+            Self::RSI => 6,
+            Self::RDI => 7,
+            Self::R(i) => i,
+        }
+    }
+}
+
+pub enum Src {
+    Reg(Reg),
+    Mem(Mem),
+    Imm(Imm),
+}
+
+pub enum Dest {
+    Reg(Reg),
+    Mem(Mem),
+}
+
+pub struct Label {
+    pub str: String,
+}
+
+pub enum AsmX86 {
+    // Stack
+    Enter(u64),
+    Leave,
+    // Copying
+    MoveQ { src: Src, dest: Dest },
+    // Control flow
+    Ret,
+    Call(Label),
+    Jmp(Label),
+    Je(Label),
+    Jne(Label),
+    // Arith/Logic
+    Add { src: Src, dest: Dest },
+    Sub { src: Src, dest: Dest },
+    IMul { src: Src, dest: Dest },
+    IDiv { div: Src },
+    Shr {reg: Reg},
+    Shl {reg: Reg},
+    Ror { src: Src, dest: Dest },
+    Cmp { src: Src, dest: Dest },
+}
